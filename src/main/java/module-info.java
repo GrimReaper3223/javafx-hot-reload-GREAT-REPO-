df9cf1ff6b1a0d737cad2802f67ab8225abc66ea
Deiv <32deiv@gmail.com>
2025-05-13 17:55:17 -0300

    [MODULE] change in the configuration of some modules

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,15 +1,18 @@
 module JavaFxHotReload {
     requires javafx.controls;
     requires javafx.fxml;
-    requires java.logging;
     requires javafx.graphics;
     requires javafx.base;
-    
-    requires atlantafx.base;
+    requires java.logging;
+
+    requires transitive org.controlsfx.controls;
+
+    requires transitive atlantafx.base;
     requires org.kordamp.ikonli.material2;
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.ikonli.core;
     requires org.kordamp.ikonli.feather;
-    
-    exports nl.scuro.tools.javafx.hotreload to javafx.graphics;
+
+    exports nl.scuro.tools.javafx.hotreload;
+    exports nl.scuro.tools.javafx.hotreload.gui;
 }
